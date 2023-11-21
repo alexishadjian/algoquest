5c27d59a84099d0b0db999e6ce7bdcfc1233104d
Alexis <alexishadjian@gmail.com>
2023-11-21 09:23:27 +0100

    Update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReponsePage from './pages/ReponsePage';
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/reponse/:id" element={<ReponsePage />} />
-    </Routes>
-  </Router>
-);
+// const App = () => (
+//   <Router>
+//     <Routes>
+//       <Route path="/reponse/:id" element={<ReponsePage />} />
+//     </Routes>
+//   </Router>
+// );
+
+// export default App;
+
+function App() {
+
+  // const [user, setUser] = useState(false);
+
+  return (
+    <div className="App">
+      
+      <RouterProvider router={
+        createBrowserRouter(
+          createRoutesFromElements(
+            
+            <Route path="/reponse/:id" element={<ReponsePage />} />
+            
+          )
+        )
+      } />
+    </div>
+  );
+}
 
 export default App;
