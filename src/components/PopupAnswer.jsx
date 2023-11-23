f64ff1888cff70b085abb0409aa0876e33c578e8
Alexis <alexishadjian@gmail.com>
2023-11-23 09:44:44 +0100

    Display several answer

--- src/components/PopupAnswer.jsx
+++ src/components/PopupAnswer.jsx
@@ -3,17 +3,27 @@ import ProgCard from '../components/ProgCard';
 
 const PopupHelp = ({isOpenAnswer, setIsOpenAnswer, datas}) => {
 
-
+  console.log(datas);
   return (
     <div className="help-popup">
       <span className="back-btn" onClick={ () => setIsOpenAnswer(!isOpenAnswer)}>{'< back'}</span>
       <h1>Answer</h1>
       <div className="help-grp">
         <div>
-          {datas.responseCardsIds.map( (cardId, index) => (
-              <div className="prog-card" key={index}>
+          {datas.responseCardsIds.map((cardId, index) => (
+            <div key={index}>
+              {Array.isArray(cardId) ? (
+                cardId.map((el, innerIndex) => (
+                  <div className="prog-card" key={innerIndex}>
+                    <ProgCard id={el} />
+                  </div>
+                ))
+              ) : (
+                <div className="prog-card" key={index}>
                   <ProgCard id={cardId} />
-              </div>
+                </div>
+              )}
+            </div>
           ))}
         </div>
       </div>
--- src/data/reponses.jsx
+++ src/data/reponses.jsx
@@ -1,13 +1,13 @@
 const reponses = [
     {
         id: 1,
-        question: "Display a result of 10 with the numbers 4 and 6",
-        responseCardsIds: [1,2,8,13,17],
+        question: "Display a result of 10 by calculating the numbers 4 and 6",
+        responseCardsIds: [[1,2,8,13,17], [1,2,8,13,17]],
     },
     {
         id: 2,
-        question: "Question 2",
-        responseCardsIds: [1,3,2],
+        question: "Display a result of 5 by calculating the numbers 8 and 3",
+        responseCardsIds: [1,3,9,12,18],
     },
 ];
 
