6b84f8d53588a15d634bccbdb6e3dc84363031e5
Alexis <alexishadjian@gmail.com>
2023-11-23 08:53:21 +0100

    Cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate } from 'react-router-dom';
 
-import ReponsePage from './pages/ReponsePage';
 import RootLayout from './layouts/RootLayout';
 
 import HomePage from './pages/Home';
 import PlayPage from './pages/Play';
-import HelpPage from './pages/Help';
 
 
 function App() {
@@ -20,8 +18,6 @@ function App() {
             <Route path="/" element={<RootLayout />}>
               <Route index element={<HomePage />} />
               <Route path="/play/:id" element={<PlayPage />} />
-              <Route path="/reponse/:id" element={<ReponsePage />} />
-              <Route path="/help" element={<HelpPage />} />
             </Route>
           )
         )
--- src/components/Reponse.jsx
+++
@@ -1,9 +0,0 @@
-const Reponse = ({ id, question, responseCardsIds }) => (
-  <div>
-    <h1>Réponse {id}</h1>
-    <h2>{question}</h2>
-    <p>{responseCardsIds}</p>
-  </div>
-);
-
-export default Reponse;
